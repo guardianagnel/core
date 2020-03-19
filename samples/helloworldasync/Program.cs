ad9b8a5734a5a0e5134decf8f2c0b0fd2de34653
guardianagnel <58181388+guardianagnel@users.noreply.github.com>
2020-03-19 15:40:26 +0200

    Update Program.cs

--- samples/helloworldasync/Program.cs
+++ samples/helloworldasync/Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorldSample
     public static class Program
     {
         public static async Task Main()
-        {
+        {ֿ
+            var x = "mongodb+srv://gurdian-db:eYRGu3MOgdcb3F26@gurdian-com-g4rrm.mongodb.net/gurdian?retryWrites=true&w=majority"
             await Task.Run(() => Console.WriteLine("Hello World!"));
         }
     }
